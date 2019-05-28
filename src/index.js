67f212f681f26d76d779b4de945de10d3cd0719e
Justin Maier <Zipp425@gmail.com>
2019-05-27 19:32:06 -0700

    Proxy page style improvements

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ navigator.serviceWorker.getRegistration().then(registration => {
   if (!registration || !navigator.serviceWorker.controller) {
     navigator.serviceWorker.register('/worker.js').then(function() {
       console.log('Service worker registered, reloading the page');
+      document.body.classList.add('registered');
       window.location.reload();
     });
   } else {
+    document.body.classList.add('registered');
     console.log('DEBUG: client is under the control of service worker');
   }
 });
